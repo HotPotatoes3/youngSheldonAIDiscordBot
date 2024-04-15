ab068419d1e3c2b30928daca5b7696fe3d9ac5fe
HotPotatoes3 <skanuparthy21@gmail.com>
2024-04-14 17:40:19 -0700

    Project initial state

---
+++ bot.py
@@ -0,0 +1,77 @@
+import os
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+import responses
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.reply(response)
+    except Exception as e:
+        return
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+def run_discord_bot(discord):
+
+    app_commands = discord.app_commands
+
+    TOKEN = os.environ['TOKEN']
+
+    client = discord.Client(intents=intents)
+    bot = commands.Bot(command_prefix="?", intents=discord.Intents.all())
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+
+
+    @bot.event
+    async def on_message(message):
+        if message.author != client.user:
+            username = str(message.author)
+            user_message = str(message.content)
+            channel = str(message.channel)
+
+            print(f"{username} said: '{user_message}' ({channel})")
+
+            await send_message(message, user_message, is_private=False)
+
+    @bot.event
+    async def on_ready():
+        print("Slash Commands working")
+        try:
+            synced = await bot.tree.sync()
+            print(f"Synced {len(synced)} command(s)")
+            for i in synced:
+                print(i)
+        except Exception as e:
+            print(e)
+
+    # responses.slash_response("askpeter", input)
+
+    @bot.tree.command(name='askpeter')
+    @app_commands.describe(input = "What do you want to ask/tell Peter?")
+    async def askpeter(interaction: discord.Interaction, input: str):
+        try:
+            await interaction.response.defer()
+            resp = responses.slash_response("askpeter", input)
+            await interaction.followup.send(resp)
+        except Exception as e:
+            print(e)
+            await interaction.response.send_message("Failed")
+
+    @bot.tree.command(name='help')
+    async def help(interaction: discord.Interaction):
+        await interaction.response.send_message('**Commands:**\n\n**?askpeter {Your question/statement}:** Responds as Peter Griffin from Family Guy')
+
+
+
+    # client.run(TOKEN)
+    bot.run(TOKEN)
---
+++ responses.py
@@ -0,0 +1,46 @@
+import os
+
+import google.generativeai as genai
+
+TOKEN2 = os.environ['TOKEN2']
+genai.configure(api_key=TOKEN2)
+
+model = genai.GenerativeModel('gemini-pro')
+
+
+
+def handle_response(message) -> str:
+    p_message = message.lower()
+
+    if p_message == '?green fn':
+        return 'https://tenor.com/view/green-fn-peter-griffin-green-gif-1521709978489846232'
+    if p_message == '?help':
+        return '**Commands:**\n\n**?askpeter {Your question/statement}:** Responds as Peter Griffin from Family Guy'
+    if p_message[0:9] == "?askpeter":
+        try:
+            response = model.generate_content(
+                'Answer the following question/statement as if you are the character Peter Griffin from the TV show "Family Guy" (try to include a cutaway gag/reference from the show in your response): ' + p_message[
+                                                                                                                                                                                                              10:],
+                safety_settings={'HARM_CATEGORY_HARASSMENT': 'block_none', 'HARM_CATEGORY_HATE_SPEECH': 'block_none',
+                                 'HARM_CATEGORY_SEXUALLY_EXPLICIT': 'block_none',
+                                 'HARM_CATEGORY_DANGEROUS_CONTENT': 'block_none'})
+
+            peterResponse = response.text
+            return peterResponse
+        except Exception as e:
+            print(e)
+            return "Please ask a question properly as follows: **?askpeter {Your question/statement}:**"
+
+def slash_response(type, input):
+    if type == 'askpeter':
+        response = model.generate_content(
+                        'Answer the following question/statement as if you are the character Peter Griffin from the TV show "Family Guy" (try to include a cutaway gag/reference from the show in your response): ' + input,
+                        safety_settings={'HARM_CATEGORY_HARASSMENT': 'block_none', 'HARM_CATEGORY_HATE_SPEECH': 'block_none',
+                                         'HARM_CATEGORY_SEXUALLY_EXPLICIT': 'block_none',
+                                         'HARM_CATEGORY_DANGEROUS_CONTENT': 'block_none'})
+
+        peterResponse = response.text
+        return peterResponse
+    elif type == 'help':
+        return '**Commands:**\n\n**?askpeter {Your question/statement}:** Responds as Peter Griffin from Family Guy'
+
